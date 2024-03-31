66cf6b917ec445410bc1eb5102a00e43ec371622
MangwonCassie <dudnfsla1@naver.com>
2024-03-31 20:55:22 +0900

    feat: UserService 로직 추가

---
+++ src/main/java/com/dailycodework/lakesidehotel/service/UserService.java
@@ -0,0 +1,27 @@
+package com.dailycodework.lakesidehotel.service;
+
+import com.dailycodework.lakesidehotel.model.User;
+
+import java.util.List;
+
+public class UserService implements IUserService {
+    @Override
+    public User registerUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public void DeleteUser(String email) {
+
+    }
+
+    @Override
+    public User getUser(String email) {
+        return null;
+    }
+}
