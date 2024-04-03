0852d0be57c47c6c29cc06dd4710515f3d2f94fe
MangwonCassie <dudnfsla1@naver.com>
2024-04-03 14:41:19 +0900

    feat: getUserByEmail 상위 exception로직 추가

--- src/main/java/com/dailycodework/lakesidehotel/controller/UserController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +21,20 @@ public class UserController {
 
     private final IUserService userService;
 
+    @GetMapping("/all")
     public ResponseEntity<List<User>> getUsers(){
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
     }
 
+    @GetMapping("/{email}")
     public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
        try {
            User theUser = userService.getUser(email);
            return ResponseEntity.ok(theUser);
        } catch (UsernameNotFoundException e){
            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+       } catch (Exception e){
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
        }
     }
 
