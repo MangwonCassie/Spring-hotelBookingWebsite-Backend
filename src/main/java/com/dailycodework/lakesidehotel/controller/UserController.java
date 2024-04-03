45e53a61375fb92255c7ea767e74a11419fd3dc2
MangwonCassie <dudnfsla1@naver.com>
2024-04-03 13:02:56 +0900

    feat: UserController Email로 User 찾는 메소드 로직 추가

--- src/main/java/com/dailycodework/lakesidehotel/controller/UserController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/UserController.java
@@ -5,6 +5,8 @@ import com.dailycodework.lakesidehotel.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,15 @@ public class UserController {
 
     public ResponseEntity<List<User>> getUsers(){
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
+    }
+
+    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
+       try {
+           User theUser = userService.getUser(email);
+           return ResponseEntity.ok(theUser);
+       } catch (UsernameNotFoundException e){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+       }
     }
 
 }
