74405ace15c8d36356d8320bc245b19fc2be5230
MangwonCassie <dudnfsla1@naver.com>
2024-04-04 14:58:56 +0900

    feat: RoleController createRole 로직 추가 및 수정

--- src/main/java/com/dailycodework/lakesidehotel/controller/RoleController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoleController.java
@@ -1,9 +1,12 @@
 package com.dailycodework.lakesidehotel.controller;
 
+import com.dailycodework.lakesidehotel.exception.RoleAlreadyExistException;
 import com.dailycodework.lakesidehotel.model.Role;
 import com.dailycodework.lakesidehotel.service.IRoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,18 @@ public class RoleController {
 
     private final IRoleService roleService;
 
-    public ResponseEntity<List<Role>> getAllRoles(){
+    public ResponseEntity<List<Role>> getRoles() {
         return new ResponseEntity<List<Role>>(roleService.getRoles(), FOUND);
+    }
+
+    public ResponseEntity<String> createRole(@RequestBody Role theRole) {
+        try {
+            roleService.createRole(theRole);
+            return ResponseEntity.ok("New role created successfully");
+        } catch (RoleAlreadyExistException re) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(re.getMessage());
+
+
+        }
     }
 }
