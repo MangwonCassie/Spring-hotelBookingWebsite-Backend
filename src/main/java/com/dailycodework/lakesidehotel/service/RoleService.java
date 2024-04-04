412d4ea9f5d294a795ad90fd46f65c7b3ae6ce9b
MangwonCassie <dudnfsla1@naver.com>
2024-04-04 12:15:54 +0900

    feat: RoleService 단 removeUserFromRole메소드

--- src/main/java/com/dailycodework/lakesidehotel/service/RoleService.java
+++ src/main/java/com/dailycodework/lakesidehotel/service/RoleService.java
@@ -6,9 +6,11 @@ import com.dailycodework.lakesidehotel.model.User;
 import com.dailycodework.lakesidehotel.repository.RoleRepository;
 import com.dailycodework.lakesidehotel.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,19 +35,29 @@ public class RoleService implements IRoleService {
     }
 
     @Override
-    public void deleteRole(Long id) {
-
+    public void deleteRole(Long roleId) {
+        this.removeAllUsersFromRole(roleId);
+        roleRepository.deleteById(roleId);
     }
 
     @Override
     public Role findByName(String name) {
-        return null;
+        return roleRepository.findByName(name).get();
     }
 
     @Override
     public User removeUserFromRole(Long userId, Long roleId) {
-        return null;
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role> role = roleRepository.findById(roleId);
+
+        if (role.isPresent() && role.get().getUsers().contains(user.get())) {
+            role.get().removeUserFromRole(user.get());
+            roleRepository.save(role.get());
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User not found");
     }
+
 
     @Override
     public User assignRoleToUser(Long userId, Long roleId) {
