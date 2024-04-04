78f11cc706e3e20e7c6ed89f2f8c997d415f2e33
MangwonCassie <dudnfsla1@naver.com>
2024-04-04 14:41:42 +0900

    feat: RoleController getAllRoles 로직 업데이트

--- src/main/java/com/dailycodework/lakesidehotel/controller/RoleController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoleController.java
@@ -1,9 +1,15 @@
 package com.dailycodework.lakesidehotel.controller;
 
+import com.dailycodework.lakesidehotel.model.Role;
 import com.dailycodework.lakesidehotel.service.IRoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.FOUND;
 
 @RestController
 @RequestMapping("/api/roles")
@@ -11,4 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController {
 
     private final IRoleService roleService;
+
+    public ResponseEntity<List<Role>> getAllRoles(){
+        return new ResponseEntity<List<Role>>(roleService.getRoles(), FOUND);
+    }
 }
