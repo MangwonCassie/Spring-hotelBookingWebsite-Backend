d0f6c5138094f3b70d2d2fab5947f82fb84dea39
MangwonCassie <dudnfsla1@naver.com>
2024-04-05 10:53:30 +0900

    feat: JwtUtils의 Logger 및 properties 설정

---
+++ src/main/java/com/dailycodework/lakesidehotel/security/jwt/JwtUtils.java
@@ -0,0 +1,14 @@
+package com.dailycodework.lakesidehotel.security.jwt;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    private String jwtSecret;
+    private int jwtExprirationTime;
+
+}
+
