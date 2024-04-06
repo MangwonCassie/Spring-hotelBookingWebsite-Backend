c874fabd111d99fb6cb02ac7dbdf943fda74a788
MangwonCassie <dudnfsla1@naver.com>
2024-04-06 21:50:16 +0900

    feat: SecurityConfig AuthentificationManager 로직 추가

--- src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
@@ -7,7 +7,9 @@ import com.dailycodework.lakesidehotel.security.user.HotelUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,4 +40,10 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 }
