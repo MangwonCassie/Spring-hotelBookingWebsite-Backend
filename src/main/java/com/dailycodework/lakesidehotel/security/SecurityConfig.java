dee8b4762c1ba0a54dd094c38a06a2cf723476c3
MangwonCassie <dudnfsla1@naver.com>
2024-04-06 21:43:16 +0900

    feat: Security config 설정 추가

--- src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
@@ -1,18 +1,41 @@
 package com.dailycodework.lakesidehotel.security;
 
 
+import com.dailycodework.lakesidehotel.security.jwt.AuthTokenFilter;
+import com.dailycodework.lakesidehotel.security.jwt.JwtAuthEntryPoint;
+import com.dailycodework.lakesidehotel.security.user.HotelUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final HotelUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+
+    @Bean
+    public AuthTokenFilter authenticationTokenFilter(){
+        return new AuthTokenFilter();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
