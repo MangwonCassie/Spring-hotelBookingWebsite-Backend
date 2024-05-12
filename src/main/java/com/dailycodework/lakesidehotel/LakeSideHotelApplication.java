0c5b5ecff2daf0d4438f53140c6786fffe3539c2
MangwonCassie <dudnfsla1@naver.com>
2024-05-12 19:49:47 +0900

    feat: 설정 파일 추가

---
+++ src/main/java/com/dailycodework/lakesidehotel/LakeSideHotelApplication.java
@@ -0,0 +1,15 @@
+package com.dailycodework.lakesidehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class LakeSideHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakeSideHotelApplication.class, args);
+    }
+
+}
