4048915535075c1ea5432d9e920bdb03ba0a136d
MangwonCassie <dudnfsla1@naver.com>
2024-05-21 13:21:17 +0900

    test: roomcontroller api추가

--- src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
@@ -34,7 +34,7 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/rooms")
+@RequestMapping("api/rooms")
 public class RoomController {
     private final IRoomService roomService;
     private final BookingService bookingService;
