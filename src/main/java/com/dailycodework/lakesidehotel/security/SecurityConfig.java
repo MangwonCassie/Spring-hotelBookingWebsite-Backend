3497765dd385feb13fd91bccec5651ddd96667f8
MangwonCassie <dudnfsla1@naver.com>
2024-05-25 21:28:31 +0900

    fix: SecurityConfig login filterchain 추가 테스트

--- src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                         exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/api/**", "/rooms/**", "/bookings/**", "/auth/**")
+                        .requestMatchers( "/api/**", "/rooms/**", "/bookings/**", "/auth/**", "/login")
                         .permitAll()
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                         .requestMatchers("/roles/**").hasRole("ADMIN")
