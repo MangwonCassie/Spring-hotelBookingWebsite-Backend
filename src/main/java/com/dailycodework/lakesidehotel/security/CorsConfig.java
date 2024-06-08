c339b99a1d15ebd0a34969136d15bcbf99dc1552
MangwonCassie <dudnfsla1@naver.com>
2024-06-08 22:48:22 +0900

    fix: allowedOrigins * 이거랑 allowCredentials 같이 못씀

--- src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
@@ -51,10 +51,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://spring-hotel-booking-website-front-git-master-yeoouls-projects.vercel.app") // 모든 출처 허용안된다고 프론트에 함께 withCredentials: true를 사용 불가
+                .allowedOrigins("*") // 모든 출처 허용안된다고 프론트에 함께 withCredentials: true를 사용 불가
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true) // 인증정보를 허용하지 않음
+                //.allowCredentials(true) // 인증정보를 허용하지 않음
                 .maxAge(3600); // 캐싱 시간 (초)
         }
     }
