3e49343270c8db00cc584edc18cf9a8b6334c62a
MangwonCassie <dudnfsla1@naver.com>
2024-06-08 22:11:38 +0900

    fix: cors에러 완전 느슨하게

--- src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
@@ -51,11 +51,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")//"Authorization", "Content-Type", "Accept"
-                .allowCredentials(true) //헐 이거 빼먹었네
-                .maxAge(3000);
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 인증정보를 허용하지 않음
+                .maxAge(3600); // 캐싱 시간 (초)
         }
     }
 
