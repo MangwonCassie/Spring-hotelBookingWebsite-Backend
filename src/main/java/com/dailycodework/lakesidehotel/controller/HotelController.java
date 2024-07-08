9505b0b98379e089d4426b828ecc6b0d11fec8ae
MangwonCassie <dudnfsla1@gmail.com>
2024-07-08 16:35:23 +0900

    feat: 3277 들어간 주소 cors에러 해제 추가

--- src/main/java/com/dailycodework/lakesidehotel/controller/HotelController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/HotelController.java
@@ -13,7 +13,7 @@ public class HotelController {
 
     private final IHotelService hotelService;
 
-    @CrossOrigin(origins = {"https://spring-hotel-booking-website-front-9dlbj6olo-yeoouls-projects.vercel.app", "http://localhost:5173/", "http://127.0.0.1:5173/", "http://127.0.0.1:5173", "https://spring-hotel-booking-website-front-git-master-yeoouls-projects.vercel.app","https://spring-hotel-booking-website-front-git-master-yeoouls-projects.vercel.app/", "https://spring-hotel-booking-website-front.vercel.app"})
+    @CrossOrigin(origins = {"https://river-hotel-91f9caaa3277.herokuapp.com","https://spring-hotel-booking-website-front-9dlbj6olo-yeoouls-projects.vercel.app", "http://localhost:5173/", "http://127.0.0.1:5173/", "http://127.0.0.1:5173", "https://spring-hotel-booking-website-front-git-master-yeoouls-projects.vercel.app","https://spring-hotel-booking-website-front-git-master-yeoouls-projects.vercel.app/", "https://spring-hotel-booking-website-front.vercel.app"})
     @PostMapping("/add")
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
         // Hotel 객체를 저장하는 서비스 호출
